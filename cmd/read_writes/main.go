3ea4e78225109cc44c32c92e212ea46cae1c7d7f
krivobokovsergej <krivobokov_sv@magnit.ru>
2023-06-19 09:21:49 +0300

    read write fix

--- cmd/read_writes/main.go
+++ cmd/read_writes/main.go
@@ -8,65 +8,31 @@ import (
 func main() {
 	cnt := 10
 	m := make(map[int]int, cnt)
-	mu := &sync.Mutex{}
 
-	counter := NewCounter(mu)
-	counter.addCount(cnt)
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
 
+	wg.Add(cnt)
 	for i := 0; i < cnt; i++ {
 		i := i
 		go func() {
-			defer counter.done()
+			defer wg.Done()
 			mu.Lock()
+			defer mu.Unlock()
 			m[i] = i
-			mu.Unlock()
 		}()
 	}
 
-	counter.addCount(cnt)
-
+	wg.Add(cnt)
 	for i := 0; i < cnt; i++ {
 		i := i
 		go func() {
-			defer counter.done()
+			defer wg.Done()
 			mu.Lock()
-			fmt.Printf("Elem: %d\n", m[i])
-			mu.Unlock()
+			defer mu.Unlock()
+			fmt.Println(m[i])
 		}()
 	}
 
-	counter.wait()
-}
-
-type Counter struct {
-	cnt int
-	mu  *sync.Mutex
-}
-
-func NewCounter(mu *sync.Mutex) *Counter {
-	counter := &Counter{}
-	counter.mu = mu
-
-	return counter
-}
-
-func (counter *Counter) addCount(cnt int) {
-	counter.mu.Lock()
-	defer counter.mu.Unlock()
-	counter.cnt = cnt + counter.cnt
-}
-
-func (counter *Counter) done() {
-	counter.mu.Lock()
-	defer counter.mu.Unlock()
-	counter.cnt--
-}
-
-func (counter *Counter) wait() {
-loop:
-	for {
-		if counter.cnt == 0 {
-			break loop
-		}
-	}
+	wg.Wait()
 }
